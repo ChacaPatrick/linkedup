0fd3f707d1a8812f26d193780c399da1bb6e08e3
a-sarkar1 <60590158+a-sarkar1@users.noreply.github.com>
2021-01-24 13:14:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
 
-  <title>Linkedup - Index</title>
+  <title>Linkedup - Login</title>
   <link rel = "icon" href = "title.png"> 
   <link rel="stylesheet" href="styles.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
