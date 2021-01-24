9b7506f9b33a7df3f8abde292edfc432e3d0808d
Patrick Chaca <40473502+ChacaPatrick@users.noreply.github.com>
2021-01-24 06:49:33 -0500

    Added split for friends php

--- friend.js
+++ friend.js
@@ -1,13 +1,35 @@
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
-      var uid = user.uid;
-      
-      // ...
-    } else {
-      // User is signed out
-      // ...
-      window.location.replace("index.php")
-    }
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        var uid = user.uid;
+        
+        /*firestore.collection('user_info').doc(uid).set({
+        
+        });*/
+        // ...
+        firestore.collection('user_info').get().then(function(querySnapshot) {
+            var para = document.createElement("li")
+            querySnapshot.forEach(function(doc) {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id, " => ", doc.data());
+                var node = document.createTextNode(doc.data()["fname"] + " " +doc.data()["lname"] )
+                para.appendChild(node);
+                var element = document.getElementById("left");
+                element.appendChild(para);
+                var br = document.createElement("br");
+                element.appendChild(br);
+                console.log(element)
+
+            });
+        });
+
+
+        } else {
+            // User is signed out
+            // ...
+            window.location.replace("index.php")
+        }
   });
+
+
