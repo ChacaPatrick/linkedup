a8e0b16874cb93e3527535cfe87e35612d3a25ab
Mtarek7900 <mohammed.tarek03@myhunter.cuny.edu>
2021-01-23 22:00:40 -0500

    put calander in home

--- home.php
+++ home.php
@@ -18,6 +18,9 @@ include 'nav.php';
 </head>
 
 <body>
+  <center>
+    <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%230B8043&amp;showTitle=0&amp;mode=WEEK&amp;showTabs=0&amp;showPrint=0&amp;showNav=1" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+  </center>
   
   <script src="scripts.js"></script>
 
